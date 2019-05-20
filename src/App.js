ab425bcb5c5a8b07fd866c4bee3d278c584d5c5f
Andrew Bellucci <andrew@bellucci.tech>
2019-05-19 22:24:46 -0400

    update on app and index

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class App extends Component{
     constructor(props){
-        super(props)
+        super(props);
+        this.state = {
+            storyIds: [],
+            currentArticles: [],
+            page: 1
+        }
     }
     
-    render(){
+    componentDidMount(){
+        
+        axios.get('https://hacker-news.firebaseio.com/v0/topstories.json')
+            .then(res => {
+                this.setState({ storyIds: res.data });
+                let stories = [];
 
+                
+                return stories;
+            })
+            .then(stories => {
+                this.setState({ articles: stories });
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    render(){
+        return(
+            <div className="container">
+                <h1>Hacker News</h1>
+                <ul>
+                    {}
+                    <li></li>
+                </ul>
+            </div>
+        );
     }
 }
 export default App;
